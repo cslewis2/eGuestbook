261fe16a4b891a85fbb487250ff4a565a9027810
cslewis2 <clarence.lewis@gmail.com>
2019-08-29 22:54:47 -0400

    fine tuned guest data entries-refactored to create seperate colums for first name,last name,address,sity and state

--- EGUEST REDO2.py
+++ EGUEST REDO2.py
@@ -1,6 +1,6 @@
 #test test testt
 #C:\Users\clarence\source\repos\eGuestbook\EGUEST REDO2.py
-##WORK FROM THIS CODE AS OF 7/28/2019
+##WORK FROM THIS CODE AS OF 7/29/2019
 from random import randint
 from datetime import date
 import csv
@@ -18,22 +18,34 @@ for i in range (0,total_guests):
         guest_date=(date.today())
         return guest_date
 
-    def guest_name():
-        guest_first=(str.capitalize(input('guest first name?  ')))
-        guest_last=(str.capitalize(input('guest last name?  ')))
-        guest_name_merge=' '.join([guest_first,guest_last])
-        return guest_name_merge #name_merge
+    #def guest_name():
+    #    guest_first=(str.capitalize(input('guest first name?  ')))
+    #    guest_last=(str.capitalize(input('guest last name?  ')))
+    #    guest_name_merge=' '.join([guest_first,guest_last])
+    #    return guest_name_merge #name_merge
 
-        def guest_fname():
-            pass
-        def guest_lname():
-            pass
+    def guest_fname():
+        guest_first=(str.capitalize(input('guest first name?  ')))           
+        return guest_first
+
+    def guest_lname():
+        guest_last=(str.capitalize(input('guest last name?  ')))        
+        return guest_last
 
     def guest_address():
         """generates guest address"""
         address=(input('what is guests street address?  '))
-        #city_state=(input('what is guest city and state?  '))
         return address
+    
+    def guest_city():
+        '''generates guest city'''
+        city=(string.capitalize(input('what city is guest from?  ')))
+        return city
+
+    def guest_state():
+        '''generates guest state'''
+        state=str.upper(input('what state is guest from?  '))
+        return state
 
     def member_name():
         """generates merged member first and last names"""
@@ -42,19 +54,19 @@ for i in range (0,total_guests):
         member_name_merge=(' '.join([member_first,member_last]))
         return member_name_merge
 
-file_exists=os.path.isfile('eGuestREDO2.csv')#
-with open('eGuestREDO.csv', 'a', newline='') as csvfile:
-#function open ('filename.CSV),r-read,w-write-a-append (add to) as filetype csvfile:
-#use with because it implicitly calls a file close for the file when done
-        fieldnames = ['Visit_Date','Guest_Name','Guest_Address','Member_Name']
-#fieldnames function defines ROW 1 in spreadsheet, column headers in single quotes in form of a list
+file_exists=os.path.isfile('eGuestREDO2.csv')
+
+with open('eGuestREDO2.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Visit_Date','Guest_Fname','Guest_Lname','Guest_Address','Guest_City','Guest_State','Member_Name']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#dictwriter is a dictionary utility function for python created csv files...transcribes a python dictionary to csv file
-        if not file_exists:#
-            writer.writeheader()#
+        if not file_exists:
+            writer.writeheader()
 
         for a in range (0,total_guests):
-                writer.writerow({'Visit_Date':visit_date(),'Guest_Name':guest_name(),\
-                'Guest_Address':guest_address(),'Member_Name':member_name()\
+                writer.writerow({'Visit_Date':visit_date(),'Guest_Fname':guest_fname(),\
+                'Guest_Lname':guest_lname(),'Guest_Address':guest_address(),\
+                'Guest_City':guest_city(),'Guest_State':guest_state(),'Member_Name':member_name()\
                 })
-#there is 1 and only 1 set of function calls at the end when writing csv to disk...otherwise will call a second #time at the end and overwrite original data input by user
+#there is 1 and only 1 set of function calls at the end of scrit when writing 
+#csv to disk...otherwise will call a second #time at the end and overwrite
+#original data input by user
