74bd837364af6d82a39e0cc58b6a30a6de69e38e
cslewis2 <clarence.lewis@gmail.com>
2024-01-24 22:51:25 -0500

    catch up

--- EGUEST REDO2a.py
+++ EGUEST REDO2a.py
@@ -11,8 +11,8 @@ layout = [
         [sg.Text('All graphic widgets in one form!', size=(30, 1), font=("Helvetica", 25))],
         [sg.Text('Here is some text.... and a place to enter text')],
         [sg.InputText('This is my text', key='in1')]]
-#sg.Window(title='HVTC eGuest Book',layout=[[sg.Button('OK')]],margins=(110,50)).read()
-sg.Window[(title='hvtc eGuest Book')],layout=[sg.Text[('Here is some text.... and a place to enter text')]]
+# sg.Window(title='HVTC eGuest Book',layout=[[sg.Button('OK')]],margins=(110,50)).read()
+sg.Window[(title='hvtc eGuest Book')],layout=[sg.Text[('Here is some text.... and a place to enter text')]],
 sg.InputText[('This is my text', key='in1')]
 #event loop
 
